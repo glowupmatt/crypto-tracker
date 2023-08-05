d5a47a888b15071468a2c1edd5d891c3a7e1297b
Matthew <glowupmatt@gmail.com>
2023-08-04 17:36:58 -0700

    updataed the api key

--- src/app/fetchApi/newsFetch.ts
+++ src/app/fetchApi/newsFetch.ts
@@ -7,12 +7,13 @@ const options = {
   params: {
     q: "Cryptocurrency",
   },
+  "X-Api-Key": "704498f5f332417f8f959c0aed695b2d",
 };
 
 export const broadCryptoNews = async (queries: string, pageSize: number) => {
   try {
     const { data } = await axios.get(
-      `${baseURL}?q=${queries}&searchIn=title,description&pageSize=${pageSize}&apiKey=704498f5f332417f8f959c0aed695b2d`
+      `${baseURL}?q=${queries}&searchIn=title,description&pageSize=${pageSize}&apiKey=${options["X-Api-Key"]}`
     );
     return data;
   } catch (err) {
